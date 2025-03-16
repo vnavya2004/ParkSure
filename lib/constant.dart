5d4a0ffeb301e07aa5fa5e20804866e68b6ff23c
vnavya2004 <118450993+vnavya2004@users.noreply.github.com>
2025-03-16 23:37:32 +0530

    Add files via upload

---
+++ lib/constant.dart
@@ -0,0 +1,8 @@
+//To store the colors used in the app
+
+import 'package:flutter/material.dart';
+
+Color kprimaryColor = Color.fromARGB(255, 1, 69, 54);
+Color ksecondaryColor = Color.fromARGB(255, 121, 118, 118);
+Color kprimaryBgColor = Colors.white;
+Color kBtnTextColor = Colors.white;
