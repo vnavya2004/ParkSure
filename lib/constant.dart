aed2f8cc3cd00f337fd3b700cfe39ed00052d58a
vnavya2004 <118450993+vnavya2004@users.noreply.github.com>
2025-03-17 17:40:51 +0530

    Delete lib directory

--- lib/constant.dart
+++
@@ -1,8 +0,0 @@
-//To store the colors used in the app
-
-import 'package:flutter/material.dart';
-
-Color kprimaryColor = Color.fromARGB(255, 1, 69, 54);
-Color ksecondaryColor = Color.fromARGB(255, 121, 118, 118);
-Color kprimaryBgColor = Colors.white;
-Color kBtnTextColor = Colors.white;
